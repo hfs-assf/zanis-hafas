0b365e8d9091786e1009d3ca7946be2d7d068039
kurnianto syaputra <kurnianto.putra@gmail.com>
2018-10-18 10:20:03 +0700

    add pelayanan medis

--- src/Component/View/PelayananMedis/soap.jsx
+++ src/Component/View/PelayananMedis/soap.jsx
@@ -4,7 +4,7 @@ const SOAP = props => (
   <div className="container-fluid">
     <div className="row soaps">
       <div className="col-md-2">
-        <div className="boxsoap">Ini Vital Sign</div>
+        <div className="boxsoap">Vital Sign</div>
         <div className="isisoap">
           <table>
             <tbody>
@@ -61,8 +61,51 @@ const SOAP = props => (
             refs="pencariansoap"
             placeholder="Cari Nama Template SOAP"
           />
-          <button className="tombolsoap">Salin</button>
+          <button className="btn btn-cyan accent-3 btn-sm ">Salin</button>
         </div>
+        <div className="row">
+          <div className="col-md-5 rowsoap">
+            <div className="legendtitle">Subjektif</div>
+            <textarea
+              placeholder="Isi Form Tindakan"
+              name="textarea"
+              class="textarea"
+            />
+          </div>
+          <div className="col-md-5 rowsoap">
+            <div className="legendtitle">Analisa</div>
+            <textarea
+              placeholder="Isi Form Tindakan"
+              name="textarea"
+              class="textarea"
+            />
+          </div>
+          <div className="col-md-5 rowsoap">
+            <div className="legendtitle">Objektif</div>
+            <textarea
+              placeholder="Isi Form Tindakan"
+              name="textarea"
+              class="textarea"
+            />
+          </div>
+          <div className="col-md-5 rowsoap">
+            <div className="legendtitle">Tindakan</div>
+            <textarea
+              placeholder="Isi Form Tindakan"
+              name="textarea"
+              class="textarea"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="col-md-12">
+        <div className="diagnosa">
+          Diagnosa <input type="text" placeholder="isi diagnosa" />
+        </div>
+      </div>
+      <div className="float-right">
+        <button className="btn btn-info btn-md">Simpan</button>
+        <button className="btn btn-orange accent-4 btn-md">Bersihkan</button>
       </div>
     </div>
   </div>
