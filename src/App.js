a0ba7709d4ead6ee692ffec59262d5d3822ad449
sucinascaisar <sucinascaisar@gmail.com>
2018-11-21 13:30:55 +0700

    merge refactor

--- src/App.js
+++ src/App.js
@@ -3,28 +3,29 @@ import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Dashboard from "./Component/JSX/DashboardHome/Dashboard";
 // Admin
-import Admin from "./Component/JSX/Admin/Dashboard";
-import KelolaKaryawan from "./Component/JSX/Admin/KelolaKaryawan";
-import KelolaPoliklinik from "./Component/JSX/Admin/KelolaPoliklinik";
-import KelolaAdminBiaya from "./Component/JSX/Admin/KelolaBiaya";
-// admin
-import Login from "./Component/JSX/Login/Login";
-import Header from "./Component/JSX/Header/Header";
-import Sidebar from "./Component/JSX/Sidebar/Sidebar";
-import Apotek from "./Component/JSX/Apotek/TimelineApotek";
-import KelolaApotek from "./Component/JSX/Apotek/KelolaApotek";
-import Pendaftaran from "./Component/JSX/Pendaftaran/Pendaftaran";
-import PendaftaranLayanan from "./Component/JSX/Pendaftaran/TambahPelayananAntrian";
-import TimelinePelayananMedis from "./Component/JSX/PelayananMedis/TimelinePelayananMedis";
-import PelayananMedisForm from "./Component/JSX/PelayananMedis/PelayananMedis";
-import Kasir from "./Component/JSX/Kasir/TimelineKasir";
-import Pembayaran from "./Component/JSX/Kasir/Pembayaran";
-// RekamMedisPasien
-import RekamMedisPasien from "./Component/JSX/PelayananMedis/RekamMedisPasien";
-import ListRekamMedis from "./Component/JSX/PelayananMedis/RekamMedislist";
-import DetailRekamMedis from "./Component/View/RekamMedis/DetailRekamMedis";
-// import KelolaBiaya from "./Component/JSX/Kasir/KelolaBiaya";
-import FakeData from "./JSON/pasien.json";
+import Admin from "./Views/JSX/Admin/Dashboard";
+import KelolaItemLaboratorium from "./Views/JSX/Admin/KelolaItemLaboratorium";
+import KelolaKaryawan from "./Views/JSX/Admin/KelolaKaryawan";
+import KelolaPoliklinik from "./Views/JSX/Admin/KelolaPoliklinik";
+import KelolaTindakan from "./Views/JSX/Admin/KelolaTindakan";
+
+// Apotek
+import KelolaApotek from "./Views/JSX/Apotek/KelolaApotek";
+import Apotek from "./Views/JSX/Apotek/TimelineApotek";
+
+// Kasir
+import Pembayaran from "./Views/JSX/Kasir/Pembayaran";
+import Kasir from "./Views/JSX/Kasir/TimelineKasir";
+
+// Pelayanan Medis
+import DaftarRekamMedis from "./Views/JSX/PelayananMedis/RekamMedis/DaftarRekamMedis";
+import DetailRekamMedis from "./Views/JSX/PelayananMedis/RekamMedis/DetailRekamMedis";
+import PelayananMedis from "./Views/JSX/PelayananMedis/PelayananMedis";
+import TimelinePelayananMedis from "./Views/JSX/PelayananMedis/TimelinePelayananMedis";
+
+//Pendaftaran
+import Pendaftaran from "./Views/JSX/Pendaftaran/Pendaftaran";
+import PendaftaranLayanan from "./Views/JSX/Pendaftaran/PendaftaranLayanan";
 
 class App extends Component {
   render() {
@@ -39,39 +40,38 @@ class App extends Component {
             <Route exact path="/" component={Dashboard} />
             <Route path="/apotek" component={Apotek} />
             <Route path="/kasir" component={Kasir} />
-<<<<<<< HEAD
             <Route
               path="/form-pembayaran/:id"
-              render={({ match }) => (
-                <Pembayaran {...FakeData.find(x => x.id === match.params.id)} />
-              )}
+              render={({ match }) => <Pembayaran antrian={match.params} />}
             />
             {/* <Route path="/kelola_biaya" component={KelolaApotek} /> */}
-=======
-            <Route path="/form-pembayaran" component={Pembayaran} />
->>>>>>> 370383111560c3aa21bbfb1eacb066421a793635
             <Route path="/kelola_apotek" component={KelolaApotek} />
             <Route path="/pendaftaran" component={Pendaftaran} />
             <Route
-              path="/tambahlayanan/:id"
+              path="/tambah-layanan/:id"
               render={({ match }) => (
                 <PendaftaranLayanan antrian={match.params} />
               )}
             />
             <Route
-              path="/formpelayananmedis/:id"
-              render={({ match }) => (
-                <PelayananMedisForm antrian={match.params} />
-              )}
+              path="/pelayanan-medis/:id"
+              render={({ match }) => <PelayananMedis antrian={match.params} />}
             />
             <Route path="/pelayanan-medis" component={TimelinePelayananMedis} />
             <Route path="/admin" component={Admin} />
             <Route path="/karyawan" component={KelolaKaryawan} />
             <Route path="/poliklinik" component={KelolaPoliklinik} />
-            <Route path="/biayatindakan" component={KelolaAdminBiaya} />
-            <Route path="/rekam_medispasien" component={RekamMedisPasien} />
-            <Route path="/lisrm" component={ListRekamMedis} />
-            <Route path="/detailrekammedis" component={DetailRekamMedis} />
+            <Route path="/tindakan" component={KelolaTindakan} />
+            <Route
+              path="/peralatan-laboratorium"
+              component={KelolaItemLaboratorium}
+            />
+            <Route path="/rekam_medis" component={DaftarRekamMedis} />
+            <Route path="/detail_rekam_medis" component={DetailRekamMedis} />
+            {/* <Route
+              path="/data_rekam_medis_pasien"
+              component={DataRekamMedisPasien}
+            /> */}
           </Switch>
         </div>
       </div>
