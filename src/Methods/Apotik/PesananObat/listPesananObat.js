3e40e81d0262d3ab3953451d35e2df5be27fd970
hafas.assaf <hafas.assaf@gmail.com>
2019-04-18 09:54:44 +0700

    update

--- src/Methods/Apotik/PesananObat/listPesananObat.js
+++ src/Methods/Apotik/PesananObat/listPesananObat.js
@@ -16,8 +16,8 @@
 import axios from "axios";
 const apiURL = "http://10.6.35.1:8005";
 
-let listPesananObat = uid => {
-  return axios.get(apiURL + "/api/v1/pesanan-obat" + uid);
+const listPesananObat = id => {
+  return axios.get(apiURL + "/api/v1/pesanan-obat" + id);
 };
 
 export default listPesananObat;
