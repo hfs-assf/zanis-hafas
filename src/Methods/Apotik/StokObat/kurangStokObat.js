2cf970bc6f60b75cc8c243fe09614532147820cd
hafas.assaf <hafas.assaf@gmail.com>
2019-07-10 16:51:37 +0700

    ganti parameter krg stokobat & masih fix bug nya

--- src/Methods/Apotik/StokObat/kurangStokObat.js
+++ src/Methods/Apotik/StokObat/kurangStokObat.js
@@ -2,10 +2,15 @@ import axios from "axios";
 
 const apiURL = "http://10.6.35.1:8005";
 
-let kurangStokObat = ({ uid, jumlah_obat, nik_dokter }) => {
-  return axios.patch(apiURL + "/api/v1/stok-obat/" + uid, {
-    jumlah_obat,
-    nik_dokter
+let kurangStokObat = ({ uid_obat, jumlah_keluar, nik_karyawan }) => {
+  console.log({
+    uid_obat,
+    jumlah_keluar,
+    nik_karyawan
+  });
+  return axios.patch(apiURL + "/api/v1/stok-obat/" + uid_obat, {
+    jumlah_keluar,
+    nik_karyawan
   });
   // console.log(uid + "---" + jumlah_obat + " -- " + nik_dokter);
 };
