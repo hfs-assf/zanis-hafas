bc8951ba8e7777be64c061abaa9025ecb67311fb
hafas.assaf <hafas.assaf@gmail.com>
2019-08-01 11:02:39 +0700

    update

--- src/Methods/Kasir/Transaksi/bayarTransaksi.js
+++ src/Methods/Kasir/Transaksi/bayarTransaksi.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import { api } from "../../api";
 
 const bayarTransaksi = ({ uid_transaksi, status, nik_kasir }) => {
-  console.log({ uid_transaksi, status, nik_kasir });
   return axios.put(api.kasir + `/transaksi/${uid_transaksi}`, {
     status,
     nik_kasir
