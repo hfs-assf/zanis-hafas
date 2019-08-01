ea658a6b754496a95b1f8c65c88887733eb57586
hafas.assaf <hafas.assaf@gmail.com>
2019-08-01 11:02:05 +0700

    update

--- src/Components/JSX/hapusModal.jsx
+++ src/Components/JSX/hapusModal.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import hapusTindakan from "../../Methods/Poli/Tindakan/hapusTindakan";
 import ModalKonfirmasi from "./Animasi/ModalKonfirmasi";
 
 class Hapus extends Component {
--- src/Methods/Kasir/Transaksi/bayarTransaksi.js
+++ src/Methods/Kasir/Transaksi/bayarTransaksi.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { api } from "../../api";
 
 const bayarTransaksi = ({ uid_transaksi, status, nik_kasir }) => {
+  console.log({ uid_transaksi, status, nik_kasir });
   return axios.put(api.kasir + `/transaksi/${uid_transaksi}`, {
     status,
     nik_kasir
