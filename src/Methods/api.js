5bf993bad5ae4ff128cd16e0040be72e18e50277
hafas.assaf <hafas.assaf@gmail.com>
2019-09-16 17:06:29 +0700

    api route

--- src/Methods/api.js
+++ src/Methods/api.js
@@ -1,16 +1,16 @@
 export const api = {
-  // membership: "http://165.22.98.116:8000/api/v1",
-  // pendaftaran: "http://165.22.98.116:8001/api/v1",
-  // rekamMedis: "http://165.22.98.116:8002/api/v1",
-  // poli: "http://165.22.98.116:8003/api/v1",
-  // kasir: "http://165.22.98.116:8004/api/v1",
-  // apotik: "http://165.22.98.116:8005/api/v1",
-  // akunUser: "http://165.22.98.116:8006/api/v1"
   membership: "http://165.22.98.116:8000/api/v1",
-  pendaftaran: "http://10.6.35.1:8001/api/v1",
-  rekamMedis: "http://10.6.35.1:8002/api/v1",
-  poli: "http://10.6.35.1:8003/api/v1",
-  kasir: "http://10.6.35.1:8004/api/v1",
-  apotik: "http://10.6.35.1:8005/api/v1",
-  akunUser: "http://10.6.35.1:8006/api/v1"
+  pendaftaran: "http://165.22.98.116:8001/api/v1",
+  rekamMedis: "http://165.22.98.116:8002/api/v1",
+  poli: "http://165.22.98.116:8003/api/v1",
+  kasir: "http://165.22.98.116:8004/api/v1",
+  apotik: "http://165.22.98.116:8005/api/v1",
+  akunUser: "http://165.22.98.116:8006/api/v1"
+  // membership: "http://165.22.98.116:8000/api/v1",
+  // pendaftaran: "http://10.6.35.1:8001/api/v1",
+  // rekamMedis: "http://10.6.35.1:8002/api/v1",
+  // poli: "http://10.6.35.1:8003/api/v1",
+  // kasir: "http://10.6.35.1:8004/api/v1",
+  // apotik: "http://10.6.35.1:8005/api/v1",
+  // akunUser: "http://10.6.35.1:8006/api/v1"
 };
