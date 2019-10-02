6aa76457cb8288670c29cff8a714c373c9dbbc96
hafas.assaf <hafas.assaf@gmail.com>
2019-10-02 15:58:00 +0700

    list pesanan

--- src/Methods/Apotik/PesananObat/listPesananObat.js
+++ src/Methods/Apotik/PesananObat/listPesananObat.js
@@ -2,7 +2,9 @@ import axios from "axios";
 import { api } from "../../api";
 
 const listPesananObat = status => {
-  return axios.get(api.apotik + "/pesanan-obat?status_pesanan=" + status);
+  return axios.get(
+    api.apotik + "/pesanan-obat?status_pesanan=" + status + "&limit=10"
+  );
 };
 
 export default listPesananObat;
