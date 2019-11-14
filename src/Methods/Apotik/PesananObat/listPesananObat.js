d268363eaced342cdc67bb6d10212a50dc858b2c
hafas.assaf <hafas.assaf@gmail.com>
2019-11-14 12:16:31 +0700

    up

--- src/Methods/Apotik/PesananObat/listPesananObat.js
+++ src/Methods/Apotik/PesananObat/listPesananObat.js
@@ -2,11 +2,9 @@ import axios from "axios";
 import { api } from "../../api";
 
 const listPesananObat = id_lokasi => {
-  return axios
-    .get(
-      `${api.apotik}/pesanan-obat?status_pesanan=MENUNGGU&limit=10&id_lokasi=${id_lokasi}`
-    )
-    .catch(err => err);
+  return axios.get(
+    `${api.apotik}/pesanan-obat?status_pesanan=MENUNGGU&limit=10&id_lokasi=${id_lokasi}`
+  );
 };
 
 export default listPesananObat;
