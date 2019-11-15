fcf21882c3e0671082557ac4642ee8d0aa359ee7
hafas.assaf <hafas.assaf@gmail.com>
2019-11-15 15:35:49 +0700

    editstatys

--- src/Methods/Kasir/Transaksi/listTransaksi.js
+++ src/Methods/Kasir/Transaksi/listTransaksi.js
@@ -3,7 +3,9 @@ import { api } from "../../api";
 
 const listTransaksi = id_lokasi =>
   axios
-    .get(`${api.kasir}/transaksi?id_lokasi=${id_lokasi}&status_pesanan=PENDING`)
+    .get(
+      `${api.kasir}/transaksi?status_transaksi=PENDING&id_lokasi=${id_lokasi}`
+    )
     .catch(err => err);
 
 export default listTransaksi;
