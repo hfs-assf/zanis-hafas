e73116b336e0a22033eef73e551e20ad92956b95
hafas.assaf <hafas.assaf@gmail.com>
2019-12-12 09:42:53 +0700

    nomor transaksi

--- src/Methods/Kasir/Transaksi/nomorTransaksi.js
+++ src/Methods/Kasir/Transaksi/nomorTransaksi.js
@@ -1,10 +1,22 @@
 import axios from "axios";
 import { api } from "../../api";
 
-let nomorTransaksi = (nomor_rm, id_lokasi) => {
-  return axios.put(
-    `${api.kasir}/nomor-transaksi/${nomor_rm}?id_lokasi=${id_lokasi}`
-  );
+let transaksiBelanja = ({
+  nik_penerbit,
+  id_lokasi,
+  nomor_rekam_medis,
+  penjamin,
+  jenis_pembayaran,
+  detailTransaksi
+}) => {
+  return axios.post(`${api.kasir}/transaksi-belanja`, {
+    nik_penerbit,
+    id_lokasi,
+    nomor_rekam_medis,
+    penjamin,
+    jenis_pembayaran,
+    detailTransaksi
+  });
 };
 
-export default nomorTransaksi;
+export default transaksiBelanja;
